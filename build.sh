5ab26e4926af599497e217a19705511872ede4ca
mritd <mritd@linux.com>
2020-06-12 14:02:40 +0800

    fix(ome-install): fix oem-install always disable systemd-resolved
    fix oem-install always disable systemd-resolved
    
    Signed-off-by: mritd <mritd@linux.com>

--- build.sh
+++ build.sh
@@ -61,7 +61,7 @@ build_rootfs() {
     ${NSPAWN} systemctl enable getty.target haveged.service systemd-networkd.service systemd-resolved.service
     ${NSPAWN} systemctl enable sshd.service zswap-arm.service bootsplash-hide-when-booted.service bootsplash-show-on-shutdown.service
     # fix manjaro-arm-oem-install always disable systemd-resolved.service
-    echo "systemctl enable systemd-resolved.service" >> ${ROOTFS_DIR}/root/.bash_profile
+    sed -i 's@systemctl disable systemd-resolved.service 1> /dev/null 2>&1@@g' ${ROOTFS_DIR}/usr/share/manjaro-arm-oem-install/manjaro-arm-oem-install
 
     msg "Applying overlay for minimal edition..."
     echo "Overlay file, just so each edition has one." > ${ROOTFS_DIR}/overlay.txt
